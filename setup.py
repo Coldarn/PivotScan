2f09ae714a8dcec0da0286caf7a067fa3778fb1c
Collin Arnold <collin.arnold@gmail.com>
2014-04-17 20:56:51 -0700

    Added py2exe setup config

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import py2exe
+
+packages = [
+    'reportlab',
+    'reportlab.graphics.charts',
+    'reportlab.graphics.samples',
+    'reportlab.graphics.widgets',
+    'reportlab.graphics.barcode',
+    'reportlab.graphics',
+    'reportlab.lib',
+    'reportlab.pdfbase',
+    'reportlab.pdfgen',
+    'reportlab.platypus',
+    'imagescanner.backends.twain',
+]
+
+setup(
+    windows=['PivotScan.pyw'],
+    data_files=['RotateLeft.png', 'RotateRight.png', 'msvcp90.dll'],
+    options={
+        'py2exe': { 'packages': packages },
+    })

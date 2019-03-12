33d206773b58afe8d6633b811ec9226e982e12d3
Collin Arnold <collin@collinarnold.net>
2019-03-11 22:18:39 -0700

    implement scanned image drag and re-center and renamed members

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,9 +17,14 @@ namespace PivotScan2
         private const string WiaFormatPNG = "{B96B3CAF-0728-11D3-9D7B-0000F81EF32E}";
 
         private Scanner scanner;
-        private PaperSize paperSize;
-        private ScannedImageSize imageSize;
-        private Point imageOffset = new Point();
+        private PaperSize pageSizeInches;
+        private ScannedImageSize imageSizeInches;
+        private Point imageOffsetInches = new Point();
+
+        // Local state for dragging the scanned image, only valid while dragging
+        private bool isDragging = false;
+        private Point dragStartPosition = new Point();
+        private Point imageOffsetStart = new Point();
 
         public MainWindow()
         {
@@ -73,14 +78,16 @@ namespace PivotScan2
             var imagePath = Path.Combine(Settings.AppDataPath, "image." + image.FileExtension);
             File.Delete(imagePath);
             image.SaveFile(imagePath);
-            this.imageSize = new ScannedImageSize
+            this.imageSizeInches = new ScannedImageSize
             {
                 Width = image.Width,
                 Height = image.Height,
                 HorizontalResolution = image.HorizontalResolution,
                 VerticalResolution = image.VerticalResolution,
             };
-            this.imageOffset = new Point();
+
+            // Center the newly-scanned image
+            this.imageOffsetInches = new Point();
 
             this.ScannedImage.Source = new ImageSourceConverter().ConvertFromString(imagePath) as ImageSource;
             this.UpdateCanvas();
@@ -89,7 +96,7 @@ namespace PivotScan2
         private void PickPaperSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var paperSize = e.AddedItems[0] as PaperSize;
-            this.paperSize = paperSize;
+            this.pageSizeInches = paperSize;
             this.UpdateCanvas();
             Settings.Instance.PaperSize = paperSize.Name;
             Settings.Instance.Save();
@@ -107,7 +114,8 @@ namespace PivotScan2
 
         private void Center_Click(object sender, RoutedEventArgs e)
         {
-
+            this.imageOffsetInches = new Point();
+            this.UpdateScannedImageBounds();
         }
 
         private void RotateRight_Click(object sender, RoutedEventArgs e)
@@ -128,34 +136,80 @@ namespace PivotScan2
         private void UpdateCanvas()
         {
             double canvasAspectRatio = this.Canvas.ActualWidth / this.Canvas.ActualHeight;
-            if (canvasAspectRatio <= this.paperSize.AspectRatio)
+
+            // Constrain the page rectangle to the available canvas area while maintaining aspect ratio
+            if (canvasAspectRatio <= this.pageSizeInches.AspectRatio)
             {
                 this.PageBackground.Width = this.Canvas.ActualWidth;
-                this.PageBackground.Height = this.Canvas.ActualWidth / this.paperSize.AspectRatio;
+                this.PageBackground.Height = this.Canvas.ActualWidth / this.pageSizeInches.AspectRatio;
             }
             else
             {
-                this.PageBackground.Width = this.Canvas.ActualHeight * this.paperSize.AspectRatio;
+                this.PageBackground.Width = this.Canvas.ActualHeight * this.pageSizeInches.AspectRatio;
                 this.PageBackground.Height = this.Canvas.ActualHeight;
             }
+            // Ensure the dashed page border mirrors the page background rectangle
             this.PageBorder.Width = this.PageBackground.Width;
             this.PageBorder.Height = this.PageBackground.Height;
 
             double topOffset = (this.Canvas.ActualHeight - this.PageBackground.Height) / 2;
-            Canvas.SetTop(this.PageBackground, topOffset);
-            Canvas.SetTop(this.PageBorder, topOffset);
-
             double leftOffset = (this.Canvas.ActualWidth - this.PageBackground.Width) / 2;
+
+            // Center the page within the canvas area
+            Canvas.SetTop(this.PageBackground, topOffset);
             Canvas.SetLeft(this.PageBackground, leftOffset);
+            Canvas.SetTop(this.PageBorder, topOffset);
             Canvas.SetLeft(this.PageBorder, leftOffset);
 
-            if (this.imageSize != null)
-            {
-                this.ScannedImage.Width = this.imageSize.Width / this.imageSize.HorizontalResolution / this.paperSize.Width * this.PageBackground.Width;
-                this.ScannedImage.Height = this.imageSize.Height / this.imageSize.VerticalResolution / this.paperSize.Height * this.PageBackground.Height;
-                Canvas.SetLeft(this.ScannedImage, -this.ScannedImage.Width / 2 + this.Canvas.ActualWidth / 2 + this.imageOffset.X);
-                Canvas.SetTop(this.ScannedImage, -this.ScannedImage.Height / 2 + this.Canvas.ActualHeight / 2 + this.imageOffset.Y);
-            }
+            this.UpdateScannedImageBounds();
+        }
+
+        private void UpdateScannedImageBounds()
+        {
+            // Only continue if we have a scanned image
+            if (this.imageSizeInches == null) return;
+
+            // Scale the scanned image to the page
+            this.ScannedImage.Width = this.imageSizeInches.Width / this.imageSizeInches.HorizontalResolution / this.pageSizeInches.Width * this.PageBackground.Width;
+            this.ScannedImage.Height = this.imageSizeInches.Height / this.imageSizeInches.VerticalResolution / this.pageSizeInches.Height * this.PageBackground.Height;
+
+            // Center it on the page, adjusting for the drag offset
+            Canvas.SetLeft(this.ScannedImage, -this.ScannedImage.Width / 2 + this.Canvas.ActualWidth / 2 + this.imageOffsetInches.X * PagePixelsPerInchX);
+            Canvas.SetTop(this.ScannedImage, -this.ScannedImage.Height / 2 + this.Canvas.ActualHeight / 2 + this.imageOffsetInches.Y * PagePixelsPerInchY);
+        }
+
+        private double PagePixelsPerInchX
+        {
+            get { return this.PageBackground.Width / this.pageSizeInches.Width; }
+        }
+        private double PagePixelsPerInchY
+        {
+            get { return this.PageBackground.Height / this.pageSizeInches.Height; }
+        }
+
+        private void Page_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.isDragging = true;
+            this.dragStartPosition = e.GetPosition(this);
+            this.imageOffsetStart = this.imageOffsetInches;
+        }
+
+        private void Page_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.isDragging = false;
+        }
+
+        private void Page_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (!this.isDragging) return;
+
+            // Total pixel offset of the mouse from the drag start
+            var dragOffsetPixels = e.GetPosition(this) - this.dragStartPosition;
+
+            // Convert the drag offset from pixels to page inches
+            var dragOffsetInches = new System.Windows.Vector(dragOffsetPixels.X / PagePixelsPerInchX, dragOffsetPixels.Y / PagePixelsPerInchY);
+            this.imageOffsetInches = this.imageOffsetStart + dragOffsetInches;
+            this.UpdateScannedImageBounds();
         }
     }
 }
